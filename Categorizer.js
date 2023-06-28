c0918df107c7b92f2f63c8901acd7ef698db357b
TonyGitGud <83114927+TonyGitGud@users.noreply.github.com>
2023-06-29 01:28:45 +1200

    All Questions List

--- Categorizer.js
+++ Categorizer.js
@@ -2,14 +2,13 @@ let items = [];
 let num_of_questions, num_of_generations, current_question = 0, current_generation = 0;
 
 function refresh(questionIndex, generatedIndex) {
-    let item = items[questionIndex], child;
+    let item = items[questionIndex];
     document.getElementById("id").innerText = `Question ID: ${item.id}`;
     document.getElementById("question").innerHTML = "";
     let question = item.question.split("\n");
-    console.log(question.length);
     for (let i = 0; i < question.length; i++) {
         document.getElementById("question").appendChild(document.createElement("h3")).innerText = question[i];
-    } 
+    }
     document.getElementById("answer").innerHTML = "";
     let answer = item.answer.split("\n");
     for (let i = 0; i < answer.length; i++) {
@@ -20,6 +19,7 @@ function refresh(questionIndex, generatedIndex) {
     for (let i = 0; i < generated.length; i++) {
         document.getElementById("generated").appendChild(document.createElement("h3")).innerText = generated[i];
     }
+    document.getElementById("generated-index").innerText = `Generation ${generatedIndex + 1}/${num_of_generations}`;
 }
 
 function prev_question() {
@@ -62,6 +62,34 @@ function next_generation() {
     }
 }
 
+function prepare_list() {
+    let question_button, question_box, item, question;
+    for (let i = 0; i < items.length; i++) {
+        item = items[i];
+        question_button = document.createElement("button");
+        question_button.innerText = `Jump to Question ${item.id}`;
+        question_button.onclick = function() {refresh(i, 0); hide_list();};
+        question_box = document.createElement("div");
+        question_box.className = "box";
+        question = item.question.split("\n");
+        for (let i = 0; i < question.length; i++) {
+            question_box.appendChild(document.createElement("h3")).innerText = question[i];
+        }
+        document.getElementById("list").appendChild(question_button);
+        document.getElementById("list").appendChild(question_box);
+    }
+}
+
+function hide_list() {
+    document.getElementById("cover").style.display = "none";
+    document.getElementById("main").style.display = "block";
+}
+
+function show_list() {
+    document.getElementById("cover").style.display = "block";
+    document.getElementById("main").style.display = "none";
+}
+
 document.onkeydown = function(e) {
     switch (e.key) {
         case "ArrowLeft":
@@ -75,13 +103,15 @@ document.onkeydown = function(e) {
     }
 }
 
-fetch("http://localhost:8080/Questions.json")
+// fetch("http://localhost:8080/Questions.json")
+fetch("Questions.json")
     .then(response => response.json())
     .then(json => {
         items = json;
         num_of_questions = items.length;
         num_of_generations = items[0].generated.length;
+        prepare_list();
         refresh(0, 0);
-        document.getElementById("cover").remove();
+        document.getElementById("loading").remove();
     })
     .catch(error => console.error(error));
